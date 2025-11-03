d8a82a86aac86241b39888a7eec9c28a4836580d
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-03 12:44:32 -0300

    Adicionado a entidade de domínio do curso.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/dominio/entidades/Curso.java
@@ -0,0 +1,90 @@
+package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.entidades;
+
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Turno;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.excecoes
+        .CodigoErro;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.excecoes
+        .ExcecaoDominio;
+
+public class Curso {
+    private Long id;
+    private String nome;
+    private ParametrosCurso parametros;
+    private Professor coordenador;
+
+    public Curso() {}
+
+    public Curso(
+            String nome,
+            ParametrosCurso parametros,
+            Professor coordenador
+    ) {
+        this.setNome(nome);
+        this.setParametros(parametros);
+        this.setCoordenadorCurso(coordenador);
+    }
+
+    public boolean validarDisciplina(Disciplina disciplina) {
+        return getParametros().validarDisciplina(disciplina);
+    }
+
+    public boolean validarTurno(Turno turno) {
+        return getParametros().validarTurno(turno);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(nome == null || nome.isBlank()) {
+            throw new ExcecaoDominio(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "Curso (ID: " + getId() + "): Validação falhou. O campo"
+                    + " obrigatório 'nome' estava nulo ou vazio."
+            );
+        }
+        this.nome = nome;
+    }
+
+    public ParametrosCurso getParametros() {
+        return parametros;
+    }
+
+    public void setParametros(ParametrosCurso parametros) {
+        if(parametros == null) {
+            throw new ExcecaoDominio(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "Curso (ID: " + getId() + "): Validação falhou. O campo"
+                    + " obrigatório 'parâmetros' estava nulo ou vazio."
+            );
+        }
+        this.parametros = parametros;
+    }
+
+    public Professor getCoordenador() {
+        return coordenador;
+    }
+
+    public void setCoordenadorCurso(Professor coordenador) {
+        if(coordenador == null) {
+            throw new ExcecaoDominio(
+                    CodigoErro.VD_001_CAMPO_OBRIGATORIO,
+                    "Curso (ID: " + getId() + "): Validação falhou. O campo"
+                    + " obrigatório 'coordenador de curso' estava nulo ou "
+                    + "vazio."
+            );
+        }
+        this.coordenador = coordenador;
+    }
+}
