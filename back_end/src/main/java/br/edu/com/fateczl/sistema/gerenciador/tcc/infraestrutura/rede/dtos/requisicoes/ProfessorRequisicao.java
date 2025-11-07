ef81878844ed5983689bc23828136ab7c49f7ba4
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-07 06:02:42 -0300

    Adicionado o record de requisição do professor.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/requisicoes/ProfessorRequisicao.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/requisicoes/ProfessorRequisicao.java
@@ -1,4 +1,13 @@
-package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos.requisicoes;
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos
+        .requisicoes;
 
-public record ProfessorRequisicao() {
-}
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .CargoProfessor;
+
+public record ProfessorRequisicao(
+        String nome,
+        String matricula,
+        String email,
+        String senha,
+        CargoProfessor cargo
+) {}
