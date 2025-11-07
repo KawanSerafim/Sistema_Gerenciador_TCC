7a4abdd0a8ec51ba9e4e4619b6b1771248b07960
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-07 03:41:18 -0300

    Adicionado o implementador de repositório do aluno.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/repositorios/implementacoes/AlunoRepositorioImpl.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/repositorios/implementacoes/AlunoRepositorioImpl.java
@@ -1,4 +1,49 @@
-package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia.mysql.repositorios.implementacoes;
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.repositorios.implementacoes;
 
-public class AlunoRepositorioImpl {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia.mysql.mapeadores.AlunoMapeador;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia.mysql.modelos.AlunoModelo;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.repositorios.spring.AlunoRepositorioSpring;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.entidades.Aluno;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas.repositorios
+        .AlunoRepositorio;
+
+import java.util.Optional;
+
+public class AlunoRepositorioImpl implements AlunoRepositorio {
+    private final AlunoRepositorioSpring repositorioSpring;
+    private final AlunoMapeador mapeador;
+
+    public AlunoRepositorioImpl(
+            AlunoRepositorioSpring repositorioSpring,
+            AlunoMapeador mapeador
+    ) {
+        this.repositorioSpring = repositorioSpring;
+        this.mapeador = mapeador;
+    }
+
+    @Override
+    public Aluno salvar(Aluno aluno) {
+        var alunoModelo = mapeador.paraModelo(aluno);
+        repositorioSpring.save(alunoModelo);
+
+        return mapeador.paraDominio(alunoModelo);
+    }
+
+    @Override
+    public Optional<Aluno> buscarPorMatricula(String matricula) {
+        Optional<AlunoModelo> alunoModeloOpt = repositorioSpring
+                .findByMatricula(matricula);
+
+        return alunoModeloOpt.map(this.mapeador::paraDominio);
+    }
+
+    @Override
+    public Optional<Aluno> buscarPorEmail(String email) {
+        Optional<AlunoModelo> alunoModeloOpt = repositorioSpring
+                .findByContaUsuarioEmail(email);
+
+        return alunoModeloOpt.map(this.mapeador::paraDominio);
+    }
 }
