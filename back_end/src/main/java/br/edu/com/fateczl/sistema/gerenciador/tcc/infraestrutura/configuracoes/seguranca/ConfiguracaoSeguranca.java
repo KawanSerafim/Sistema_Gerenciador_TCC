136dfb67d540c44e6c8cfe8d15e3a4de03d30487
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-07 06:41:20 -0300

    Adicionado a configuração de segurança do sistema, que por enquanto só aborda sobre o endpoint de cadastrar professor.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/ConfiguracaoSeguranca.java
@@ -0,0 +1,31 @@
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.configuracoes
+        .seguranca;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers
+        .AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ConfiguracaoSeguranca {
+
+    @Bean
+    public SecurityFilterChain filtroChaveSeguranca(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(autorizacao -> autorizacao
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/professores/api/cadastrar"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
