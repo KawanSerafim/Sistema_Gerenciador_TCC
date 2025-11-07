7530a2aff39229138aa7b7f0585feed39af95e6f
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-07 02:47:30 -0300

    Adicionado o modelo de dados da turma.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/modelos/TurmaModelo.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/modelos/TurmaModelo.java
@@ -1,7 +1,8 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
         .mysql.modelos;
 
-import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
 import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Turno;
 import jakarta.persistence.*;
 
