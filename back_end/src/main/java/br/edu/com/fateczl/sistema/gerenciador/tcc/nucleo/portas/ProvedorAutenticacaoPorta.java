fe70a1bd2d707fbd18affcdebe3f2e885fe58987
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 03:34:05 -0300

    Adicionado a porta abstrata para a infra implementar a autenticação de login.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/ProvedorAutenticacaoPorta.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/ProvedorAutenticacaoPorta.java
@@ -1,5 +1,9 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas;
 
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas.excecoes.ExcecaoContaInvalida;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas.excecoes.ExcecaoCredencialInvalida;
+
 public interface ProvedorAutenticacaoPorta {
-    void autenticar(String email, String senha);
+    void autenticar(String email, String senha)
+            throws ExcecaoCredencialInvalida, ExcecaoContaInvalida;
 }
