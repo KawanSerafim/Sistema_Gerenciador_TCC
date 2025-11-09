464ed33e54071078d42c3155f24422f7266292fa
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 04:47:36 -0300

    Bean do PasswordEncoder adicionado na configuração de segurança.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/ConfiguracaoSeguranca.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/ConfiguracaoSeguranca.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication
+        .configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers
         .AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -41,5 +44,10 @@ public class ConfiguracaoSeguranca {
             AuthenticationConfiguration configuracaoAutenticacao
     ) throws Exception {
         return configuracaoAutenticacao.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
