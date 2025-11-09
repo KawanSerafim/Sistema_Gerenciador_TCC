a517bf741bd95879d318b7da729b32667e98278b
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 05:50:50 -0300

    Porta para o remetente de email adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/RemetenteEmailPorta.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/RemetenteEmailPorta.java
@@ -1,5 +1,5 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas;
 
-public interface RemetenteEmail {
+public interface RemetenteEmailPorta {
     void enviarEmail(String para, String assunto, String corpo);
 }
