6053caa6514c5627bc9bac4c7fa03d93f7e3e092
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 06:38:06 -0300

    Caso de uso de enviar email de confirmação adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/EnviarEmailConfirmacaoCaso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/EnviarEmailConfirmacaoCaso.java
@@ -1,6 +1,6 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.casosdeuso;
 
-public interface EnviarEmailCaso {
+public interface EnviarEmailConfirmacaoCaso {
     record Entrada(String email) {}
 
     void executar(Entrada entrada);
