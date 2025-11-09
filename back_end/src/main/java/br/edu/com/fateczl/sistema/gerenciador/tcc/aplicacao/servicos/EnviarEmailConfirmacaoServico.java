48f5431a67758a598fc67ccabbeddf0a6ebd785d
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 06:31:08 -0300

    Serviço para enviar email de confirmação adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/aplicacao/servicos/EnviarEmailConfirmacaoServico.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/aplicacao/servicos/EnviarEmailConfirmacaoServico.java
@@ -1,4 +1,66 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.aplicacao.servicos;
 
-public class EnviarEmailConfirmacaoServico {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.casosdeuso
+        .EnviarEmailCaso;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.entidades
+        .ContaUsuario;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas
+        .CacheCodigoPorta;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas
+        .RemetenteEmailPorta;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class EnviarEmailConfirmacaoServico implements EnviarEmailCaso {
+    private final CacheCodigoPorta cacheCodigo;
+    private final RemetenteEmailPorta remetenteEmail;
+
+    public EnviarEmailConfirmacaoServico(
+            CacheCodigoPorta cacheCodigo,
+            RemetenteEmailPorta remetenteEmail
+    ) {
+        this.cacheCodigo = cacheCodigo;
+        this.remetenteEmail = remetenteEmail;
+    }
+
+    @Override
+    public void executar(Entrada entrada) {
+        ContaUsuario.validarFormatoEmail(entrada.email());
+
+        String codigo = gerarCodigo();
+        String assunto = "Confirme seu Email";
+        String corpo = gerarCorpoEmail(codigo);
+
+        cacheCodigo.colocarCodigo(codigo, entrada.email());
+        remetenteEmail.enviarEmail(entrada.email(), assunto, corpo);
+    }
+
+    private String gerarCodigo() {
+        int tamanho = 6;
+        Random random = new Random();
+        StringBuilder codigo = new StringBuilder(tamanho);
+        String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        for(int i = 0; i < tamanho; i++){
+            codigo.append(
+                    CARACTERES.charAt(random.nextInt(CARACTERES.length()))
+            );
+        }
+
+        return codigo.toString();
+    }
+
+    private String gerarCorpoEmail(String codigo) {
+        return
+            "Olá e seja bem-vindo!\n\n"
+            + "Você está a um passo de concluir o seu cadastro, e para isso "
+            + "é preciso inserir o token abaixo no formulário.\n\n"
+            + "Token: " + codigo + "\n\n"
+            + "Atenção: Este token de confirmação é válido por apenas "
+            + "15 minutos. Se ele expirar, será preciso solicitar um novo "
+            + "token.";
+    }
 }
