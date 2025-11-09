ceeca0a03f7870f0aec04418cf71cdee221fa206
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 04:44:47 -0300

    Adapdator do criptografo de senhas adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/criptografia/CriptografoSenhasAdaptador.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/criptografia/CriptografoSenhasAdaptador.java
@@ -1,4 +1,25 @@
-package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.configuracoes.seguranca.criptografia;
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.configuracoes
+        .seguranca.criptografia;
 
-public class CriptografoSenhasAdaptador {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas.CriptografoSenhasPorta;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CriptografoSenhasAdaptador implements CriptografoSenhasPorta {
+    private final PasswordEncoder encoder;
+
+    public CriptografoSenhasAdaptador(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @Override
+    public String criptografar(String senha) {
+        return this.encoder.encode(senha);
+    }
+
+    @Override
+    public boolean comparar(String senhaRecebida, String senhaCadastrada) {
+        return this.encoder.matches(senhaRecebida, senhaCadastrada);
+    }
 }
