f451745c89608b2ab3a619103399c3ba5fed2f9e
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 06:34:20 -0300

    Record de requisição de email de confirmação adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/requisicoes/EmailConfirmacaoRequisicao.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/requisicoes/EmailConfirmacaoRequisicao.java
@@ -1,3 +1,4 @@
-package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos.requisicoes;
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos
+        .requisicoes;
 
 public record EmailConfirmacaoRequisicao(String email) {}
