feb14052ba559f06e505866ac0344df74ce9b492
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 05:40:35 -0300

    Adaptador da porta de cache de códigos adicionado.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/adaptadores/CacheCodigoAdaptador.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/adaptadores/CacheCodigoAdaptador.java
@@ -1,4 +1,29 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.adaptadores;
 
-public class CacheCodigoAdaptador {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas
+        .CacheCodigoPorta;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+
+import java.util.Optional;
+
+public class CacheCodigoAdaptador implements CacheCodigoPorta {
+    private static final String NOME_CACHE = "codigosEmail";
+
+    @Override
+    @CachePut(value = NOME_CACHE, key = "#codigo")
+    public String colocarCodigo(String codigo, String email) {
+        return email;
+    }
+
+    @Override
+    @Cacheable(value = NOME_CACHE, key = "#codigo")
+    public Optional<String> pegarEmailPeloCodigo(String codigo) {
+        return Optional.empty();
+    }
+
+    @Override
+    @CacheEvict(value = NOME_CACHE, key = "#codigo")
+    public void removerCodigo(String codigo) {}
 }
