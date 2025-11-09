a2aefb1d09380cb3253a0ec9782182a87b59f141
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 02:39:58 -0300

    Adicionado a anotação @Service na classe.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/autenticacao/ServicoDetalhesUsuarioImpl.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/autenticacao/ServicoDetalhesUsuarioImpl.java
@@ -14,10 +14,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class ServicoDetalhesUsuarioImpl implements UserDetailsService {
     private final AdministradorRepositorio administradorRepositorio;
     private final ProfessorRepositorio professorRepositorio;
