d9717dd38d1967737cb504e9d0e5365d55e0f422
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 21:09:23 -0300

    Adicionado o caso de uso para gerar curso.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/GerarCurso.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/GerarCurso.java
@@ -1,4 +1,31 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.casosdeuso;
 
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.entidades
+        .Professor;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Turno;
+
+import java.util.List;
+
 public interface GerarCurso {
+    record Entrada(
+            String nome,
+            List<Turno> turnos,
+            List<Disciplina> disciplinas,
+            Integer maxAlunosGrupo,
+            Professor coordenadorCurso
+    ) {}
+
+    record Saida(
+            Integer id,
+            String name,
+            List<Turno> turnos,
+            List<Disciplina> disciplinas,
+            Integer maxAlunosGrupo,
+            Professor coordenadorCurso
+    ) {}
+
+    Saida executar(Entrada entrada);
 }
