105f23bda6b73ac6a0160953aadb93f4e91f9f48
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 22:09:20 -0300

    Adicionado o implementador do repositório do curso.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/repositorios/implementacoes/CursoRepositorioImpl.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/persistencia/mysql/repositorios/implementacoes/CursoRepositorioImpl.java
@@ -1,4 +1,46 @@
-package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia.mysql.repositorios.implementacoes;
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.repositorios.implementacoes;
 
-public class CursoRepositorioImpl {
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.mapeadores.CursoMapeador;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.modelos.CursoModelo;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.persistencia
+        .mysql.repositorios.spring.CursoRepositorioSpring;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.entidades
+        .Curso;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.portas.repositorios
+        .CursoRepositorio;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class CursoRepositorioImpl implements CursoRepositorio {
+    private final CursoRepositorioSpring repositorioSpring;
+    private final CursoMapeador mapeador;
+
+    public CursoRepositorioImpl(
+            CursoRepositorioSpring repositorioSpring,
+            CursoMapeador mapeador
+    ) {
+        this.repositorioSpring = repositorioSpring;
+        this.mapeador = mapeador;
+    }
+
+    @Override
+    public Curso salvar(Curso curso) {
+        var cursoModelo = mapeador.paraModelo(curso);
+        repositorioSpring.save(cursoModelo);
+
+        return mapeador.paraDominio(cursoModelo);
+    }
+
+    @Override
+    public Optional<Curso> buscarNome(String nome) {
+        Optional<CursoModelo> cursoModeloOpt = repositorioSpring
+                .findByNome(nome);
+
+        return cursoModeloOpt.map(this.mapeador::paraDominio);
+    }
 }
