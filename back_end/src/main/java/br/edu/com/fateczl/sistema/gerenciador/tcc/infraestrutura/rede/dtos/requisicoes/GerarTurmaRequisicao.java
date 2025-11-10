c63649770387504c9d17541213e001878a1d1ac1
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-10 17:39:23 -0300

    Adicionado o record de requisição para gerar turma.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/requisicoes/GerarTurmaRequisicao.java
@@ -0,0 +1,16 @@
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos
+        .requisicoes;
+
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Turno;
+
+public record GerarTurmaRequisicao(
+        String emailCoordenador,
+        Disciplina disciplina,
+        Turno turno,
+        String matriculaProfessorTg,
+        Integer ano,
+        Integer semestre
+) {}
