091685681a898b8d0aca93b85643ba5573e4fdf7
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-09 22:59:03 -0300

    Adicionado a anotação @EnableMethodSecurity para permissões de acesso a endpoints privados.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/ConfiguracaoSeguranca.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/configuracoes/seguranca/ConfiguracaoSeguranca.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication
         .configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration
+        .EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers
         .AbstractHttpConfigurer;
@@ -17,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableCaching
+@EnableMethodSecurity
 public class ConfiguracaoSeguranca {
 
     @Bean
