66605d1c8e6b19d5587af5ef97809d32f0f3ee28
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-12 20:42:17 -0300

    Adicionado a porta abstrata para o leitor de arquivos de dados dos alunos.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/LeitorArquivoDadosAlunosPorta.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/portas/LeitorArquivoDadosAlunosPorta.java
@@ -5,7 +5,7 @@ import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Turno;
 import java.io.InputStream;
 import java.util.List;
 
-public interface LeitorArquivoDadosAlunos {
+public interface LeitorArquivoDadosAlunosPorta {
     record DadosAlunos(String name, String matricula) {}
 
     record DadosArquivo(
