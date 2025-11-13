161456d5993680a8bc8d7e724e9842eeeb78c052
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-12 21:04:15 -0300

    Adicionado o record de resposta para exportar alunos.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/respostas/ExportarAlunosResposta.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/dtos/respostas/ExportarAlunosResposta.java
@@ -1,7 +1,8 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos
         .respostas;
 
-import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
 import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.Turno;
 
 public record ExportarAlunosResposta(
