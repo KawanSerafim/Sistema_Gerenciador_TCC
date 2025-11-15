ddf0baf7a4b729df83e7f5065b1872f9fe24bf30
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-15 01:04:01 -0300

    Adicionado o enum para os tipos de TCC.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/dominio/enums/TipoTcc.java
@@ -0,0 +1,8 @@
+package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums;
+
+public enum TipoTcc {
+    DESENVOLVIMENTO_SOFTWARE,
+    MONOGRAFIA,
+    ARTIGO,
+    PROJETO_NEGOCIO
+}
