df8f07e19615529ddf412830712a452362b2f3b9
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-17 12:21:56 -0300

    Adicionado o caso de uso para gerar grupos de TCC.

--- back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/GerarGrupoTcc.java
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/nucleo/casosdeuso/GerarGrupoTcc.java
@@ -1,4 +1,19 @@
 package br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.casosdeuso;
 
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums
+        .Disciplina;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.dominio.enums.TipoTcc;
+
+import java.util.List;
+
 public interface GerarGrupoTcc {
+    record Entrada(
+            String tema,
+            TipoTcc tipoTcc,
+            Disciplina disciplina,
+            List<String> matriculasAluno,
+            Long idTurma
+    ) {}
+
+    void executar(Entrada entrada);
 }
