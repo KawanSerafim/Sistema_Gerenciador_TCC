1e7a8381d02f280baeeccec577a1e3c0444d9fe6
Kawan Serafim de Souza <kawanserafimdesouza.job@gmail.com>
2025-11-17 21:25:14 -0300

    Adicionado o controlador dos grupos de TCC.

---
+++ back_end/src/main/java/br/edu/com/fateczl/sistema/gerenciador/tcc/infraestrutura/rede/controladores/GrupoTccControlador.java
@@ -0,0 +1,38 @@
+package br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede
+        .controladores;
+
+import br.edu.com.fateczl.sistema.gerenciador.tcc.infraestrutura.rede.dtos
+        .requisicoes.GerarGrupoTccRequisicao;
+import br.edu.com.fateczl.sistema.gerenciador.tcc.nucleo.casosdeuso
+        .GerarGrupoTccCaso;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/grupo-tcc/api")
+@CrossOrigin(origins = "*")
+public class GrupoTccControlador {
+    private final GerarGrupoTccCaso gerarGrupoTccCaso;
+
+    public GrupoTccControlador(GerarGrupoTccCaso gerarGrupoTccCaso) {
+        this.gerarGrupoTccCaso = gerarGrupoTccCaso;
+    }
+
+    @PostMapping("/gerar")
+    public ResponseEntity<String> gerar(
+            @RequestBody GerarGrupoTccRequisicao requisicao
+    ) {
+        var entrada = new GerarGrupoTccCaso.Entrada(
+                requisicao.tema(),
+                requisicao.tipoTcc(),
+                requisicao.disciplina(),
+                requisicao.matriculasAluno(),
+                requisicao.idTurma()
+        );
+
+        gerarGrupoTccCaso.executar(entrada);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
